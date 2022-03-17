b74aa7fe15c731b3dfe487b373351cd3d7c58700
Ali-Ahmad2002 <aliahmad.557755@gmail.com>
2022-03-17 18:45:46 +0100

    show current city
    Co-authored-by: Cagri Avsar <CagriAvsar@users.noreply.github.com>

---
+++ src/app/data-service.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataServiceService {
+
+  key: string = 'acc636805422f66eae7c098211b0a208';
+  city: string = 'Berlin';
+
+  days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+  currentWeather: any = [];
+  weeklyWeather: any = [];
+
+  constructor() { }
+
+  getCurrentWeather() {
+    return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&APPID=${this.key}&units=metric`);
+  }
+
+  getWeeklyWeather() {
+    return fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${this.city}&appid=${this.key}&units=metric`);
+  }
+
+
+  loadCurrentData() {
+    this.getCurrentWeather().then(async (response: any) => {
+      let responseAsJson = await response.json();
+      console.log('current', responseAsJson);
+      this.currentWeather.push(responseAsJson);
+    });
+  }
+
+  loadWeeklyWeather() {
+    this.getWeeklyWeather().then(async (response: any) => {
+      let responseAsJson = await response.json();
+      this.weeklyWeather.push(responseAsJson);
+      console.log('weekly', this.weeklyWeather);
+    })
+  }
+}
---
+++ src/app/weather-data/weather-data.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { DataServiceService } from '../data-service.service';
+
+@Component({
+  selector: 'app-weather-data',
+  templateUrl: './weather-data.component.html',
+  styleUrls: ['./weather-data.component.scss']
+})
+export class WeatherDataComponent implements OnInit {
+
+
+
+  constructor(public service: DataServiceService) { }
+
+  ngOnInit(): void {
+    this.service.loadCurrentData();
+    this.service.loadWeeklyWeather();
+  }
+
+  searchCities(id: any) {
+    console.log('city', id);
+    this.service.currentWeather = [];
+    this.service.weeklyWeather = [];
+    this.service.city = id;
+    this.service.loadCurrentData();
+    this.service.loadWeeklyWeather();
+  }
+
+
+}
