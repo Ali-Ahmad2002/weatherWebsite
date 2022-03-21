148b8a28ad936cedee5ea0e9d4300fdcbcef39c5
Ali-Ahmad2002 <aliahmad.557755@gmail.com>
2022-03-21 14:49:15 +0100

    show-Temps

--- src/app/data-service.service.ts
+++ src/app/data-service.service.ts
@@ -9,29 +9,27 @@ export class DataServiceService {
   key: string = 'acc636805422f66eae7c098211b0a208';
   city: string = 'Berlin';
 
-  days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
   currentWeather: any = [];
   weeklyWeather: any = [];
 
   constructor() { }
 
-  // getCurrentWeather() {
-  //   return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&APPID=${this.key}&units=metric`);
-  // }
+  getCurrentWeather() {
+    return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&APPID=${this.key}&units=metric`);
+  }
 
   getWeeklyWeather() {
     return fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${this.city}&appid=${this.key}&units=metric`);
   }
 
 
-  // loadCurrentData() {
-  //   this.getCurrentWeather().then(async (response: any) => {
-  //     let responseAsJson = await response.json();
-  //     console.log('current', responseAsJson);
-  //     this.currentWeather.push(responseAsJson);
-  //   });
-  // }
+  loadCurrentData() {
+    this.getCurrentWeather().then(async (response: any) => {
+      let responseAsJson = await response.json();
+      console.log('current', responseAsJson);
+      this.currentWeather.push(responseAsJson);
+    });
+  }
 
   loadWeeklyWeather() {
     this.getWeeklyWeather().then(async (response: any) => {
--- src/app/weather-data/weather-data.component.ts
+++ src/app/weather-data/weather-data.component.ts
@@ -1,3 +1,4 @@
+import { getLocaleDateFormat, getLocaleDayNames } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { DataServiceService } from '../data-service.service';
 
@@ -13,17 +14,58 @@ export class WeatherDataComponent implements OnInit {
   constructor(public service: DataServiceService) { }
 
   ngOnInit(): void {
-    // this.service.loadCurrentData();
+    this.service.loadCurrentData();
     this.service.loadWeeklyWeather();
   }
 
   searchCities(id: any) {
     console.log('city', id);
-    // this.service.currentWeather = [];
+    this.service.currentWeather = [];
     this.service.weeklyWeather = [];
     this.service.city = id;
-    // this.service.loadCurrentData();
+    this.service.loadCurrentData();
     this.service.loadWeeklyWeather();
+  }
+
+  showCurrentWeather(temp: any) {
+    return temp.toFixed(0);
+  }
+
+  showTempMax(maxTemp: any) {
+    return maxTemp.toFixed(0);
+  }
+
+  showMinTemp(minTemp: any) {
+    return minTemp.toFixed(0);
+  }
+
+  showDay(day: any) {
+
+
+    let currentDay = new Date().getTime();
+    let pastDay = currentDay - new Date().getTime();
+    let test = pastDay * new Date().getDay();
+    console.log(test);
+    // return this.getDays(currentDate);
+  }
+
+  getDays(currentDate: any) {
+    if (currentDate == 1) {
+      return 'Mo'
+    } else if (currentDate == 2) {
+      return 'Tue'
+    } else if (currentDate == 3) {
+      return 'Wed'
+    } else if (currentDate == 4) {
+      return 'Thu'
+    } else if (currentDate == 5) {
+      return 'Fr'
+    } else if (currentDate == 6) {
+      return 'Sat'
+    } else if (currentDate == 7) {
+      return 'Sun'
+    }
+    return currentDate;
   }
 
 
