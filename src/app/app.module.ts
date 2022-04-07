254464059ee849d81e3bfdeebaa5806cc95494a9
Ali-Ahmad2002 <aliahmad.557755@gmail.com>
2022-04-07 03:16:37 +0200

    chart-notComplete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatInputModule} from '@angular/material/input';
-import {MatIconModule} from '@angular/material/icon';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';	
-import {MatButtonModule} from '@angular/material/button';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { NgChartsModule } from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WeatherDataComponent } from './weather-data/weather-data.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChartDataComponent } from './chart-data/chart-data.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeatherDataComponent
+    WeatherDataComponent,
+    ChartDataComponent,
   ],
   imports: [
     BrowserModule,
@@ -25,7 +29,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatIconModule,
     FormsModule,
     ReactiveFormsModule,
-    MatButtonModule
+    MatButtonModule,
+    NgChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
